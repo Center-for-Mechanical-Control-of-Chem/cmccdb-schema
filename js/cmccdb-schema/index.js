05d52d78e8f1e7862c3eb3ac5e41994c948056a4
b3m2a1 <b3m2a1@gmail.com>
2025-09-22 20:30:07 -0500

    patch js field names

--- js/cmccdb-schema/index.js
+++ js/cmccdb-schema/index.js
@@ -15,9 +15,8 @@
  */
 
 "use strict";
-import * as reaction_json from '.reaction_pb.json'
 module.exports = {
     ...require('./proto/dataset_pb'),
     ...require('./proto/reaction_pb'),
-    "reaction_json":reaction_json
+    "reaction_json":require('./reaction_pb.json')
 };
